6bec24241c1d4acc8fb54d2d55906ece7a903cc4
David Nachman <david@nachman.org>
2017-05-17 21:14:33 -0400

    starting login form

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDbwpp2jgUhSerlknmslCiw392wDXPzDfw',
+      authDomain: 'udemy-reactnative-auth.firebaseapp.com',
+      databaseURL: 'https://udemy-reactnative-auth.firebaseio.com',
+      projectId: 'udemy-reactnative-auth',
+      storageBucket: 'udemy-reactnative-auth.appspot.com',
+      messagingSenderId: '30703267482'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text> An App </Text>
+        <LoginForm />
       </View>
     );
-
-  };
+  }
 }
 
 export default App;
