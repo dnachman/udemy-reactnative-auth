b0fc0efe70dd6fc982292ab9273afd49e27aa1c8
David Nachman <david@nachman.org>
2017-05-26 15:22:32 -0400

    made logged in screen

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDbwpp2jgUhSerlknmslCiw392wDXPzDfw',
@@ -14,13 +15,38 @@ class App extends Component {
       storageBucket: 'udemy-reactnative-auth.appspot.com',
       messagingSenderId: '30703267482'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+       return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
